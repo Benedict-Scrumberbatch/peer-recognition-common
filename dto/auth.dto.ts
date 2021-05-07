49e12be4dffdd823afd25163b6c931c28ba810cb
Sahil Malhotra <sahilmalhotra164@gmail.com>
2021-05-07 09:55:25 -0400

    auth dto

---
+++ dto/auth.dto.ts
@@ -0,0 +1,11 @@
+export class athDtoFull {
+    access_token: string;
+    refresh_token: string;
+    accessTokenExpire: string;
+    refreshTokenExpire: string;
+}
+
+export class authDto {
+    access_token: string;
+    accessTokenExpire: string;
+}
