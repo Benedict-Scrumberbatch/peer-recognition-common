45a8dd2b6915d258750f7d9816a66309b3516287
goldenwo <68965162+goldenwo@users.noreply.github.com>
2021-05-07 13:38:43 -0400

    auth dto

--- dto/auth.dto.ts
+++ dto/auth.dto.ts
@@ -1,11 +1,19 @@
+<<<<<<< Updated upstream
 export class authDtoFull {
+=======
+export class authDtoFull{
+>>>>>>> Stashed changes
     access_token: string;
     refresh_token: string;
     accessTokenExpire: string;
     refreshTokenExpire: string;
 }
 
+<<<<<<< Updated upstream
 export class authDto {
+=======
+export class authDto{
+>>>>>>> Stashed changes
     access_token: string;
     accessTokenExpire: string;
 }
